a4c45bcd981f59c6bbce8eec62bb94a0042afacb
VishalPhirkoj <vishalphirkoj13@gmail.com>
2021-11-15 09:39:20 +0530

    minicard.js

---
+++ src/Minicard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Main.css";
+
+function Minicard(props) {
+  return (
+    <>
+      <div className="card bg-dark text-white">
+        <img src={props.img} className="card-img" alt={props.alt} />
+        <div className="card-img-overlay ">
+          <p className="card-text mt-5 pt-4 text-center miniTitle">
+            {props.title}
+          </p>
+          <p className="text-center  miniPosts">{props.posts} Posts</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Minicard;
