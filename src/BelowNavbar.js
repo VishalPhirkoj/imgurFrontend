dd43a466d2c57bc898ff541de0a846e8c65325a0
VishalPhirkoj <vishalphirkoj13@gmail.com>
2021-11-21 18:12:07 +0530

    navbar

---
+++ src/BelowNavbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function NewPost() {
+  return (
+    <>
+      <div className="row">
+        <div className="col-sm-12">
+          <div className="card">
+            <div className="row">
+              <div className="col-sm-6">Drag image here</div>
+              <div className="col-sm-6">Choose photo</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NewPost;
