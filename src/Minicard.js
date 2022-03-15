f556f0fcacb3d9067231126e8576922967ad7b0c
VishalPhirkoj <77499770+VishalPhirkoj@users.noreply.github.com>
2022-03-15 19:16:54 +0530

    Update Minicard.js

--- src/Minicard.js
+++ src/Minicard.js
@@ -8,6 +8,7 @@ function Minicard(props) {
         <img src={props.img} className="card-img" alt={props.alt} />
         <div className="card-img-overlay ">
           <p className="card-text mt-5 pt-4 text-center miniTitle">
+            //for title
             {props.title}
           </p>
           <p className="text-center  miniPosts">{props.posts} Posts</p>
